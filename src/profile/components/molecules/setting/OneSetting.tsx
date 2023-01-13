3b8f356d84058d8e22a7facf7822f12a470b8b2e
daepang <dev.daepang@gmail.com>
2023-01-13 14:23:30 +0900

    Setting 구현

--- src/profile/components/molecules/setting/OneSetting.tsx
+++ src/profile/components/molecules/setting/OneSetting.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const OneSetting = () => {
+interface Props {
+  id: number;
+  name: string;
+  isChecked: boolean;
+  handleChange: Function;
+}
+
+const OneSetting = ({ id, name, isChecked, handleChange }: Props) => {
   return (
     <>
       <div className={classNames('setting-switch')}>
-        <span className={classNames('title')}>{'알림설정'}</span>
+        <span className={classNames('title')}>{name}</span>
         <span className={classNames('form__checkbox-switch')}>
-          <input type='checkbox' id={'switch'} className={classNames('checkbox-switch')} />
-          <label htmlFor={'switch'} className={classNames('label-switch')}>
+          <input
+            type='checkbox'
+            id={'switch' + id}
+            className={classNames('checkbox-switch')}
+            checked={isChecked}
+            onChange={name === '전체 알림 설정' ? e => handleChange(e) : e => handleChange(e, id)}
+          />
+          <label htmlFor={'switch' + id} className={classNames('label-switch')}>
             알림체크
           </label>
         </span>
