879eb4011d6dd2c5378197c01c4ee0646af6fc83
daepang <dev.daepang@gmail.com>
2023-01-19 15:16:22 +0900

    친구헤더 추가

---
+++ src/common/components/header/HeaderFriend.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface Props {
+  addFriend?: Function;
+}
+
+const Header = ({ addFriend }: Props) => {
+  return (
+    <>
+      <header className={classNames('module-header')}>
+        <div className={classNames('module-header-layer')}>
+          <h1 className={classNames('module-header-title')}>친구 목록</h1>
+          <div className={'module-header-right'}>
+            <a className={'module-header-btn module-header-btn-add'} onClick={addFriend && (() => addFriend())}>
+              친구 추가
+            </a>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
