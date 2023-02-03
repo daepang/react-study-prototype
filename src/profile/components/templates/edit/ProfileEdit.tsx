ec49fdd4cdbe2749d8b6ab59aeb2887462a363b2
daepang <dev.daepang@gmail.com>
2023-02-03 22:22:01 +0900

    ProfileEdit.tsx

--- src/profile/components/templates/edit/ProfileEdit.tsx
+++ src/profile/components/templates/edit/ProfileEdit.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import ProfileImage from 'src/profile/components/molecules/ProfileImage';
+
 const ProfileEdit = () => {
-  return <></>;
+  return (
+    <>
+      <ProfileImage imageUrl={'https://placeimg.com/200/100/animals/sepia'} />
+    </>
+  );
 };
 
 export default ProfileEdit;
