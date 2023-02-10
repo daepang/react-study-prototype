0f9f67dae1355209151c1dd27862a41e6d7e631b
daepang <dev.daepang@gmail.com>
2023-02-10 22:16:30 +0900

    ProfileEdit 하단 버튼 추가

--- src/pages/profile/edit/index.tsx
+++ src/pages/profile/edit/index.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import PageLayout from 'src/common/components/PageLayout';
 
 import ProfileEdit from 'src/profile/components/templates/edit/ProfileEdit';
+import ProfileSaveButton from 'src/profile/components/molecules/edit/ProfileSaveButton';
 
 const Index = () => {
   return (
-    <PageLayout headerTitle={'프로필 편집'}>
+    <PageLayout headerTitle={'프로필 편집'} bottomButton={<ProfileSaveButton />}>
       <ProfileEdit />
     </PageLayout>
   );
