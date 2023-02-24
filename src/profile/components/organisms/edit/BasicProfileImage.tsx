2097ece9d9181e91ff154fa67f0e02be28152254
daepang <dev.daepang@gmail.com>
2023-02-25 00:03:52 +0900

    BasicProfileImage.tsx

--- src/profile/components/organisms/edit/BasicProfileImage.tsx
+++ src/profile/components/organisms/edit/BasicProfileImage.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 
+import { imageUrlToBase64 } from 'src/common/util/base64';
+
 interface Props {
+  // 기본 이미지 페이지 열림,닫힘 상태 변경
   setIsOpenBasicPage: Function;
+  // 이미지 편집 레이어 열림,닫힘 상태 변경
   setIsOpenImageEdit: Function;
 }
 
 const BasicProfileImage = ({ setIsOpenBasicPage, setIsOpenImageEdit }: Props) => {
   // 이미지 선택 여부
   const [isSelect, setIsSelect] = useState<boolean>(false);
+
+  // 이미지 클릭 호출
+  const handleImageClick = () => {
+    setIsOpenImageEdit(false);
+    setIsOpenBasicPage(false);
+  };
 
   return (
     <>
