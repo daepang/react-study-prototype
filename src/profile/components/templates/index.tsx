d4e7b302f53975fe96ae1bb5c0d537fa017dfeb4
daepang <dev.daepang@gmail.com>
2023-02-27 16:28:38 +0900

    프로필 메인 이미지 URL 변경

--- src/profile/components/templates/index.tsx
+++ src/profile/components/templates/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { RootState } from 'src/store';
 
 import ProfileInfo from 'src/profile/components/organisms/ProfileInfo';
 
 const Profile = () => {
+  // 프로필 이미지 상태
+  const imageUrl = useSelector((state: RootState) => state.profile.imageUrl);
+
   return (
     <>
       {/* 프로필 정보 영역 */}
-      <ProfileInfo imageUrl={'https://picsum.photos/100/100'} />
+      <ProfileInfo imageUrl={imageUrl} />
     </>
   );
 };
