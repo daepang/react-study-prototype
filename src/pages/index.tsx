855656c3bbf145e54db5b0ed016d548bd84cdf48
daepang <dev.daepang@gmail.com>
2023-03-10 10:34:28 +0900

    index 페이지 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Index = () => {
   return (
     <>
-      <h1>React Study Main Page</h1>
+      <section style={{ padding: '50px' }}>
+        <h1 style={{ textAlign: 'center', fontSize: '28px' }}>React 학습용 프로젝트</h1>
+
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <Link href={'/profile'}>
+            <a style={{ fontSize: '20px' }}>👉 프로젝트 살펴보기</a>
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
